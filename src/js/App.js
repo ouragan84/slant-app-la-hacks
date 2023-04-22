51affbf0bf302b6bf1b14161630fac41ca97d2d1
Ani Gokul <anigokul@Anis-MacBook-Air-2.local>
2023-04-22 07:35:17 -0700

    inital; asd

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,47 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import ReactDOM from "react-dom";
+import FileManager from "./components/FileManager";
 
 export default () => {
+    const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
+
+    let textEditorStyle = {
+        inner:{
+            width: '612pt',
+            height:'100%',
+            border: '0px',
+            boxShadow: "0px 0px 10px #ddd"
+        },
+        outer:{
+            display:'flex',
+            alignItems: "center",
+            justifyContent: "center",
+            height:windowSize.current[1],
+        }
+    }
 
     return (
         <>  
-            <h1 style={titleStyle}>
-                Hello from ReactJS! pop
-            </h1>
-            <textarea id="file-input" style={{height: '50pt'}}/>
-            <br/>
+            <div>
+                <div style={{
+                    backgroundColor:'#eeefff',
+                    float:'left',
+                    width:'20%',
+                    height:windowSize.current[1],
+                    border:0,
+                }}>
+                <FileManager/>
+                </div>
+                <div style={textEditorStyle.outer}>
+                    <textarea id="file-input" style={textEditorStyle.inner}/>
+                </div>
+            </div>
+                
+
+            {/* <br/>
             <button id="load-file-button">Load File</button>
-            <button id="save-file-button">Save File</button>
+            <button id="save-file-button">Save File</button> */}
         </>
     )
 };
---
+++ src/js/components/FileManager.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect, useRef} from "react";
+
+export default () => {
+
+    return (
+        <>  
+            <p style={{fontSize:30, fontFamily: 'Open Sans'}}>Anton Petrov</p>
+        </>
+    )
+}
